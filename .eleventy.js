32baa6dfd7aa37b0722c080b7f5d86fa1a5338cc
Dietmar <dietmar.gigler@reinblau.de>
2021-07-20 14:41:04 +0200

    updates, removed img-plugin

--- .eleventy.js
+++ .eleventy.js
@@ -3,25 +3,46 @@ const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const htmlmin = require("html-minifier");
 const markdownIt = require("markdown-it");
-/* const Image = require("@11ty/eleventy-img");
 
-async function imageShortcode(src, alt, sizes) {
+/* const Image = require("@11ty/eleventy-img")
+const path = require('path')
+
+async function imageShortcode(src, alt, style) {
+  let sizes = "(min-width: 1024px) 100vw, 50vw"
+  let srcPrefix = `./src`
+  src = srcPrefix + src
+  console.log(`Generating image(s) from:  ${src}`)
+  if(alt === undefined) {
+    // Throw an error on missing alt (alt="" works okay)
+    throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`)
+  }  
   let metadata = await Image(src, {
-    widths: [357, 700, 1217],
-    formats: ["webp", "jpeg", "svg"]
-  });
+    widths: [350 ,600, 900, 1280],
+    formats: ['webp', 'jpeg'],
+    urlPath: "/images/",
+    outputDir: "./_site/images/",
 
-  let imageAttributes = {
-    alt,
-    sizes,
-    loading: "lazy",
-    decoding: "async",
-  };
-
-  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
-  return Image.generateHTML(metadata, imageAttributes, {
-    whitespaceMode: "inline"
-  });
+    filenameFormat: function (id, src, width, format, options) {
+      const extension = path.extname(src)
+      const name = path.basename(src, extension)
+      return `${name}-${width}w.${format}`
+    }
+  })  
+  let lowsrc = metadata.jpeg[0]
+  let highsrc = metadata.jpeg[metadata.jpeg.length - 1]  
+  return `<picture>
+    ${Object.values(metadata).map(imageFormat => {
+      return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`
+    }).join("\n")}
+    <img
+      src="${lowsrc.url}"
+      width="${highsrc.width}"
+      height="${highsrc.height}"
+      alt="${alt}"
+      class="${style}"
+      loading="lazy"
+      decoding="async">
+  </picture>`
 } */
 
 module.exports = function (eleventyConfig) {
@@ -83,9 +104,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/static/fonts");
 
   // eleventy-img
-  // eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
-  // eleventyConfig.addLiquidShortcode("image", imageShortcode);
-  // eleventyConfig.addJavaScriptFunction("image", imageShortcode);
+  // eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode)
+  // eleventyConfig.addJavaScriptFunction("image", imageShortcode)
 
   // Minify HTML
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
